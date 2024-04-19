c6c01535f2335f7c142edd1565f8e1c2f620bca6
malte <malteut@gmail.com>
2024-04-19 09:41:10 +0200

    aosirten

--- config/nvim/lua/vimtex-config.lua
+++ config/nvim/lua/vimtex-config.lua
@@ -2,7 +2,8 @@ vim.cmd("let g:vimtex_view_method = 'zathura'")
 vim.cmd('let g:vimtex_syntax_conceal_disable = 1')
 vim.cmd("let g:vimtex_compiler_latexmk = { 'build_dir' : 'build', }")
 
-vim.cmd('autocmd FileType tex inoremap ,sl \\begin{frame}<Enter>\\frametitle{}<Enter><Enter>(<>)<Enter><Enter>\\end{frame}<Enter><Enter>(<>)<Esc>6kf}i')
+vim.cmd(
+  'autocmd FileType tex inoremap ,sl \\begin{frame}<Enter>\\frametitle{}<Enter><Enter>(<>)<Enter><Enter>\\end{frame}<Enter><Enter>(<>)<Esc>6kf}i')
 
 vim.cmd('autocmd FileType tex inoremap ,mt \\begin{matrix}  \\end{matrix}<Esc>Bhi')
 vim.cmd('autocmd FileType tex inoremap ,mp \\begin{pmatrix}  \\end{pmatrix}<Esc>Bhi')
@@ -28,13 +29,17 @@ vim.cmd('autocmd FileType tex inoremap <C-s> <Esc>:w<CR>a')
 vim.cmd('autocmd FileType tex inoremap ,w <Esc>A')
 vim.cmd('autocmd FileType tex inoremap ,r $$<Esc>i')
 vim.cmd('autocmd FileType tex inoremap ,tc \\mathcal{}<Esc>i')
-vim.cmd('autocmd FileType tex inoremap ,tb \\mathbb{}<Esc>i')
+vim.cmd('autocmd FileType tex inoremap ,tm \\mathbb{}<Esc>i')
+vim.cmd('autocmd FileType tex inoremap ,tb \\textbf{}<Esc>i')
 vim.cmd('autocmd FileType tex inoremap ,tx \\text{}<Esc>i')
+vim.cmd('autocmd FileType tex inoremap ,tt \\texttt{}<Esc>i')
+vim.cmd('autocmd FileType tex inoremap ,la \\label{}<Esc>i')
 vim.cmd('autocmd FileType tex inoremap ,_ _{}<Esc>i')
 vim.cmd('autocmd FileType tex inoremap ,h ^{}<Esc>i')
 vim.cmd('autocmd FileType tex inoremap ,nc \\newcommand\\{(<>)}<Esc>5hi')
 
-vim.cmd('autocmd FileType tex inoremap ,im \\begin{figure}[h]<Enter>\\includegraphics[]{}<Enter>\\centering<Enter>\\end{figure}<Esc>2kf{a')
+vim.cmd(
+  'autocmd FileType tex inoremap ,im \\begin{figure}[h]<Enter>\\includegraphics[]{}<Enter>\\centering<Enter>\\end{figure}<Esc>2kf{a')
 
 vim.cmd('inoremap <C-@> <C-Space>')
 vim.cmd('autocmd FileType tex inoremap <C-Space> <Esc>/(<>)<CR>:noh<CR>cf)')
